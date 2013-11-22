805003353d8a6adee585cde89174fa42576dd248
Laurence <djroa1985@gmail.com>
2013-11-22 19:53:23 +0000

    Updated login form

--- kommuteV01/app/controllers/Application.java
+++ kommuteV01/app/controllers/Application.java
@@ -8,13 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index(){
-        return ok(index.render("This is a template."));
-    }
-    public static Result testing(){
-    	return ok("This is a test");
-    }
-    public static Result testing2(){
-    	return ok("This is testing no2");
+        return ok(index.render("This is the logo."));
     }
 
 }
