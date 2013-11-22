1619618e956b6958fe220f7ea22bcfc261ed012e
Laurence <djroa1985@gmail.com>
2013-11-22 16:10:45 +0000

    This is testing uri with testing2 with the action testing2

--- kommuteV01/app/controllers/Application.java
+++ kommuteV01/app/controllers/Application.java
@@ -13,5 +13,8 @@ public class Application extends Controller {
     public static Result testing(){
     	return ok("This is a test");
     }
+    public static Result testing2(){
+    	return ok("This is testing no2");
+    }
 
 }
